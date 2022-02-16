d8140727abfafcdd3f119602b1c6c227e4caa5e1
valeri <valeri.koort@punkfinance.ee>
2022-02-17 00:29:50 +0200

    feat: improve readability of total minutes count

--- 2021-01_training/trained.py
+++ 2021-01_training/trained.py
@@ -30,6 +30,10 @@ def make_bar(height):
     return f'<div class="bar" style="height: {height_}px"></div>'
 
 
+def readable_minutes(m):
+    return f"{m//60}h{m - m//60 * 60}m"
+
+
 def get_main_html():
     start = time.monotonic()
     html = '<table class="bordered">\n\n'
@@ -39,10 +43,10 @@ def get_main_html():
 
     counters = {person: len([line for line in lines.values() if line]) for person, lines in trainings.items()}
     minutes = {person: get_minutes(lines) for person, lines in trainings.items()}
-    minutes_total = {person: sum(x for x in m if x) for person, m in minutes.items()}
+    minutes_total = {person: readable_minutes(sum(x for x in m if x)) for person, m in minutes.items()}
     max_minutes = max(max(x for x in m if x is not None) for m in minutes.values())
 
-    html += '<tr><th style="border: 0"></th>' + ''.join(f'<th>{person} &middot; {counters[person]} &middot; Σ={minutes_total[person]}</th>' for person in trainings) + '</tr>\n\n'
+    html += '<tr><th style="border: 0"></th>' + ''.join(f'<th>{person} &middot; {counters[person]} &middot; {minutes_total[person]}</th>' for person in trainings) + '</tr>\n\n'
 
     strut_bar = f'<div class="bar" style="height: {max_minutes // BAR_SCALE}px; width: 0;"></div>'
     sparklines = {person: make_sparkline(minutes[person]) for person in trainings}
